28ad2a88d6ce384b90f4468b04708c777d8bd909
Ricardo Mendes [1201779] <1201779@isep.ipp.pt>
2021-03-02 10:57:06 +0000

    Add maven

---
+++ src/main/java/com/mendes/app/App.java
@@ -0,0 +1,13 @@
+package com.mendes.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
